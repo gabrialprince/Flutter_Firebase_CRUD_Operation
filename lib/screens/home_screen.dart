28d504998821082d089e097382eb86576aefb37f
gabrialprince <gabrielprince2k@gmail.com>
2022-09-27 12:28:25 +0530

    some Minor Changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
     showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
-              title: const Text("From StartupProjects"),
+              title: const Text("Alert"),
               content: Text(msg),
               actions: <Widget>[
                 // ignore: deprecated_member_use
